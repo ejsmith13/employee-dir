922cdf88d8cbb925550e39db909abb0e0a84be97
ejsmith13 <ejsmithwork@gmail.com>
2021-04-13 22:17:43 -0400

    package json updates

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./style.css";
 
 function Button(props) {
-  // props.results.name.sort();
-  
-  
   return (
-    <div className= "buttonDiv">
-        <p>Click to order last names alphabetically  </p>
-      <button className="btn btn-primary" onClick={props.handleOrder}>Reorder</button>
+    <div className="buttonDiv">
+      <p>Click to order last names alphabetically </p>
+      <button className="btn btn-primary" onClick={props.handleOrder}>
+        Reorder
+      </button>
     </div>
   );
 }
